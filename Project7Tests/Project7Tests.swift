ecb712627d8f0b3dc075f60163598f81ecec3d0d
JackG <jacques.gonzalez@me.com>
2016-02-10 18:28:59 +0100

    Initial Commit

---
+++ Project7Tests/Project7Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Project7Tests.swift
+//  Project7Tests
+//
+//  Created by Jacques on 10/02/16.
+//  Copyright © 2016 J4SOFT. All rights reserved.
+//
+
+import XCTest
+@testable import Project7
+
+class Project7Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
